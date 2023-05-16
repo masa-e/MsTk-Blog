50cd87f3ae7e387055ac27fc18c16647532ce713
masa-e <lucas.hutha358@gmail.com>
2023-05-16 23:07:03 +0900

    プロトタイプ作成

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+import tailwindTypography from "@tailwindcss/typography";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["nuxt-microcms-module", "@nuxtjs/tailwindcss"],
+    microCMS: {
+        serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+        apiKey: process.env.MICROCMS_API_KEY,
+    },
+    tailwindcss: {
+      config: {
+        content: [],
+        plugins: [tailwindTypography],
+      },
+    },
+})
